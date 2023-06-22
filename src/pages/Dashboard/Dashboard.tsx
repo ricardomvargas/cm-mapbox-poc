52ddb96afda4a3f8fc43e6d06b8d07dd759debc1
Ricardo Moura Vargas <mvargas.ricardo@gmail.com>
2023-06-22 15:21:54 +0200

    adding WMS layers for ESPG:28992

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
-import { MapProvider } from '../../context/mapContext/MapContext';
+// import { MapProvider } from '../../context/mapContext/MapContext';
 
-import MapContainer from '../../components/mapContainer/MapContainer';
-import Sidebar from '../../components/sidebar/Sidebar';
+// import MapContainer from '../../components/mapContainer/MapContainer';
+// import Sidebar from '../../components/sidebar/Sidebar';
 
 import './Dashboard.css';
 
 const Dashboard: React.FC = () => (
-  <MapProvider>
-    <article className='dashboard'>
-      <Sidebar />
-      <section className='map-section'>
-        <MapContainer />
-      </section>
-    </article>
-  </MapProvider>
+  <div>test</div>
+  // <MapProvider>
+  //   <article className='dashboard'>
+  //     <Sidebar />
+  //     <section className='map-section'>
+  //       <MapContainer />
+  //     </section>
+  //   </article>
+  // </MapProvider>
 );
 
 export default Dashboard;
